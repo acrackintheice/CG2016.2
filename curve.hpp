8d8c050f081caedf019e151152ab1200a86f8ebb
Eduardo Demeneck Onghero <acrackintheice@gmail.com>
2016-10-05 00:37:05 -0300

    Update curve.hpp

--- curve.hpp
+++ curve.hpp
@@ -1,19 +1,18 @@
-#ifndef CURVE_HPP
-#define CURVE_HPP
+#ifndef CURVE_H_
+#define CURVE_H_
 
-#include "coordinates.hpp"
 #include "object.hpp"
-#include <string>
-#include <vector>
+#include "matriz4x1.hpp"
+#include "matriz4x4.hpp"
+#include "matriz1x4.hpp"
 
-
-class Curve: public Object
-{
+class Curve : public Object{
 public:
-    Curve(std::vector<Coordinates> points, std::string name, Color* color);
-    ~Curve();
-    std::vector<Coordinates> get_drawing_points();
-    void clip();
+	Curve(std::vector<Coordinates> points, std::string name, Color* color);
+	virtual ~Curve();
+private:
+	void blending_functions();
+	void clip();
 };
 
-#endif // LINE_HPP
+#endif /* CURVE_H_ */
